4c981d665632bd39e9e5a554992b23f24510222e
Andrew Nesbitt <andrewnez@gmail.com>
2016-12-06 10:18:44 +0000

    Add restart task to capistrano

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,1 +1,10 @@
 server '163.172.159.178', user: 'root', roles: %w{app}
+
+namespace :deploy do
+  task :restart do
+    on roles(:app) do |host|
+      execute "restart watcher"
+    end
+  end
+  after :publishing, :restart
+end
