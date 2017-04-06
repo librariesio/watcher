cef0167b4747d57ef49807c6528fbd425517590e
Andrew Nesbitt <andrewnez@gmail.com>
2017-04-06 17:53:17 +0100

    Push to critical queue

--- watcher.rb
+++ watcher.rb
@@ -31,7 +31,7 @@ def follow_feed(url, platform)
             end
             if name
               puts "#{platform}/#{name}"
-              Sidekiq::Client.push('queue' => 'default', 'class' => 'PackageManagerDownloadWorker', 'args' => [platform, name])
+              Sidekiq::Client.push('queue' => 'critical', 'class' => 'PackageManagerDownloadWorker', 'args' => [platform, name])
             end
           rescue => exception
             p entry
@@ -95,7 +95,7 @@ def follow_json(url, platform)
 
       (names - update_names).each do |name|
         puts "#{platform}/#{name}"
-        Sidekiq::Client.push('queue' => 'default', 'class' => 'PackageManagerDownloadWorker', 'args' => [platform, name])
+        Sidekiq::Client.push('queue' => 'critical', 'class' => 'PackageManagerDownloadWorker', 'args' => [platform, name])
       end
 
       dc.set(url, names)
